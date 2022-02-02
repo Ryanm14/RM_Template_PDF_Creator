352ec814f79d0589f3d5bdcca10567d2ae02f5d7
Ryan Miles <ryanm1114@gmail.com>
2022-02-02 18:02:16 -0500

    init code

---
+++ main.js
@@ -0,0 +1,30 @@
+const {PDFDocument, StandardFonts, rgb} = PDFLib
+
+async function createPdf() {
+  // Create a new PDFDocument
+  const pdfDoc = await PDFDocument.create()
+  var myUploadedFile = document.getElementById("myFile").files[0];
+  const existingPdfBytes = await myUploadedFile.arrayBuffer()
+  // return
+  //
+  // const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+  document.getElementById("button").innerText = "Creating PDF! (This might take a minute)"
+  const [uploadedPdfDoc] = await pdfDoc.embedPdf(existingPdfBytes);
+
+  const pages = document.getElementById("numPages").value
+  for (let i = 0; i < pages; i++) {
+    const page = pdfDoc.addPage();
+    page.drawPage(uploadedPdfDoc)
+  }
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save()
+
+  document.getElementById("button").innerText = "PDF Created! Downloading!"
+
+  // Trigger the browser to download the PDF document
+  download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+}
+
+document.getElementById("button").onclick = () => createPdf()
